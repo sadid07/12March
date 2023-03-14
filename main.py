87615287f714c2edfeacfd86b7add74fbc85d74d
Sadid Bin Hasan <60701052+sadid07@users.noreply.github.com>
2023-03-15 03:54:48 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Order Agnostic Binary Search
+
+#def binary_search(arr, key):
+#    return arr.index(key)
+
+#print(binary_search([4,6,10],10))
+
+#one_down
+
+#learned best with PRAMP
+#with Peer Practice
+#with Sharing
+num = 2012
+num1=num // 10
+print(num1//10)
